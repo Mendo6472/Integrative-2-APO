4ea5ca50a6165b0236e8a3bb6e3d2c987a818e31
Santiago050704 <svalenciagarcia707@gmail.com>
2023-05-04 16:04:13 -0500

    Project folders and classes are uploaded.

---
+++ src/model/Order.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Date;
+import datastructures.Pair;
+
+public class Order {
+  
+  private String buyerName;
+  private ArrayList<Pair<String, Integer>> productsList;
+  private double totalPrice;
+  private Date date;
+
+  public Order() {
+    
+  }
+
+  public String getBuyerName() {
+    return buyerName;
+  }
+
+  public void setBuyerName(String buyerName) {
+    this.buyerName = buyerName;
+  }
+
+  public ArrayList<Pair<String, Integer>> getProductsList() {
+    return productsList;
+  }
+
+  public void setProductsList(ArrayList<Pair<String, Integer>> productsList) {
+    this.productsList = productsList;
+  }
+
+  public double getTotalPrice() {
+    return totalPrice;
+  }
+
+  public void setTotalPrice(double totalPrice) {
+    this.totalPrice = totalPrice;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+
+}

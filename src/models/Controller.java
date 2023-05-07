0ffcf83f36353c0cd3e3afd2faf6f1e70e517a77
Alejolonber25 <alejandrolonber25@gmai.com>
2023-05-06 23:19:01 -0500

    Add products and orders implemented

--- src/models/Controller.java
+++ src/models/Controller.java
@@ -1,10 +1,52 @@
 package models;
 import java.util.ArrayList;
-
+import java.util.Date;
+import com.google.gson.reflect.TypeToken;
+import datastructure.Pair;
 public class Controller {
 
   private ArrayList<Product> products;
   private ArrayList<Order> orders;
+  private Connection<Product> connectionProducts = new Connection<>("src/data/products.json", new TypeToken<ArrayList<Product>>(){}.getType());
+  private Connection<Order> connectionOrders = new Connection<>("src/data/orders.json", new TypeToken<ArrayList<Order>>(){}.getType());
+
+  public Controller(){
+    products = new ArrayList<>();
+    orders = new ArrayList<>();
+    products = connectionProducts.getEntity();
+    orders = connectionOrders.getEntity();
+  }
+
+  public void addProduct(String name, String description, double price, int availableQuantity, Category category) {
+    Product product;
+    int productExist = searchProduct(name);
+    if (productExist != -1) {
+      product = products.get(productExist);
+      product.setAvailableQuantity(product.getAvailableQuantity()+availableQuantity);
+    } else {
+      product = new Product(name, description, price, availableQuantity, category);
+      products.add(product);
+    }
+    connectionProducts.updateEntity(products);
+  }
+
+  public void addOrder(String buyerName, ArrayList<Pair<String,Integer>> productsList, double totalPrice, Date purchaseDate) {
+    Order order = new Order(buyerName, productsList, totalPrice, purchaseDate);
+    orders.add(order);
+    connectionProducts.updateEntity(products);
+    connectionOrders.updateEntity(orders);
+  }
+
+  public int searchProduct(String name) {
+    int itWasFound = -1;
+    for (int i = 0; i < products.size(); i++) {
+      if (products.get(i).getName().equals(name)) {
+        itWasFound = i;
+        break;
+      }
+    }
+    return itWasFound;
+  }
 
   public ArrayList<Product> getProducts() {
     return products;
